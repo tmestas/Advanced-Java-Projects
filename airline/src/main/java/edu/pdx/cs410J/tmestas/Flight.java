796d3dd765032173072f305b75b124128e92e2a2
Tanner <tmestas@pdx.edu>
2023-01-17 17:41:41 -0800

    Added functionality for flight class

--- airline/src/main/java/edu/pdx/cs410J/tmestas/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/tmestas/Flight.java
@@ -3,6 +3,22 @@ package edu.pdx.cs410J.tmestas;
 import edu.pdx.cs410J.AbstractFlight;
 
 public class Flight extends AbstractFlight {
+  private int FlightNumber;
+  private String Source;
+  private String DepartureString; //date and time
+  private String Destination;
+  private String ArrivalString;   //date and time
+
+  public Flight(){
+
+  }
+  public Flight(int flightNumber, String source, String departureString, String destination, String arrivalString){
+    this.FlightNumber = flightNumber;
+    this.Source = source;
+    this.DepartureString = departureString;
+    this.Destination = destination;
+    this.ArrivalString = arrivalString;
+  }
   @Override
   public int getNumber() {
     return 42;
@@ -26,5 +42,14 @@ public class Flight extends AbstractFlight {
   @Override
   public String getArrivalString() {
     throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  public void displayAll(){
+    System.out.println("FLIGHT #" + FlightNumber + ": ");
+    System.out.println();
+    System.out.println("Source Airport: " + Source);
+    System.out.println("Depart Date and Time: " + DepartureString);
+    System.out.println("Destination Airport: " + Destination);
+    System.out.println("Arrival Date and Time: " + ArrivalString);
   }
 }
--- airline/src/main/java/edu/pdx/cs410J/tmestas/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/tmestas/Project1.java
@@ -13,7 +13,8 @@ public class Project1 {
   }
 
   public static void main(String[] args) {
-    Flight flight = new Flight();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+
+    //Flight flight = new Flight();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
 
     if(args == null) //if there are no args
     {
@@ -21,14 +22,39 @@ public class Project1 {
       System.err.println("There are no args included (probably put usage here in the future");
     }
 
+
     /*
     for (String arg : args) { //is printing all args individually, recognizes quotes as 1 arg
       System.out.println(arg);
     }
     */
 
+    int flightNum = 0;
+    String source = null;
+    String departureString = null;
+    String destination = null;
+    String arrivalString = null;
+
+    int i = 0;
+
+    while(i < args.length){
+      if(i == 0){flightNum = 99;}
+      if(i == 1){source = args[i];}
+      if(i == 2){departureString = args[i];}
+      if(i == 3){destination = args[i];}
+      if(i == 4){arrivalString = args[i];}
+      ++i;
+    }
+
+    Flight flight = new Flight(flightNum, source, departureString, destination, arrivalString);
+
+    flight.displayAll();
+
+    /*
     Airline newAirline = new Airline(args[0]);
-    System.out.println(newAirline.getName());
+    System.out.println(newAirline.getName()); //successfully adds name to newAirline object
+    */
+
 
   }
 
