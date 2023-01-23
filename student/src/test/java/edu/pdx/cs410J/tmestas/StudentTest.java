5ccf819f637454d16c7ba05762c12615905575ec
Tanner <tmestas@pdx.edu>
2023-01-22 16:05:06 -0800

    Rollback student project changes

--- student/src/test/java/edu/pdx/cs410J/tmestas/StudentTest.java
+++ student/src/test/java/edu/pdx/cs410J/tmestas/StudentTest.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs410J.tmestas;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -21,11 +22,23 @@ public class StudentTest
     var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
     assertThat(pat.getName(), equalTo(name));
   }
-
   @Test
   void allStudentsSayThisClassIsTooMuchWork(){
     Student student = new Student("Tanner", new ArrayList<>(), 0.0, "IDK");
     assertThat(student.says(), equalTo("This class is too much work!"));
+  }
+
+  @Test
+  @Disabled
+  void daveStudentSaysWhatIsExpected(){
+    ArrayList<String> classes = new ArrayList<>();
+    classes.add("Algorithm");
+    classes.add("Operating Systems");
+    classes.add("Java");
+    Student dave = new Student("Dave", classes, 3.64, "Male");
+    String daveString = dave.toString();
+
+    assertThat(daveString, equalTo("Dave has a GPA of 3.64 and is taking 3 classes: Algorithms, Operating Systems, and Java. He says \"This class is too much work\"."));
   }
 
 }
