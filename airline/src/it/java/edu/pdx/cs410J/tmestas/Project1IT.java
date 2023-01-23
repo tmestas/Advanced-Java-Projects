4405e4f491f68e91ad7ec9130e563c7739b4bfa7
Tanner <tmestas@pdx.edu>
2023-01-23 12:12:03 -0800

    Fixed implementation test

--- airline/src/it/java/edu/pdx/cs410J/tmestas/Project1IT.java
+++ airline/src/it/java/edu/pdx/cs410J/tmestas/Project1IT.java
@@ -24,7 +24,7 @@ class Project1IT extends InvokeMainTestCase {
   @Test
   void testNoCommandLineArguments() {
     MainMethodResult result = invokeMain();
-    assertThat(result.getTextWrittenToStandardError(), containsString("\nNOT ENOUGH ARGUMENTS INCLUDED\n\n" +
+    assertThat(result.getTextWrittenToStandardError(), containsString("\n\nNOT ENOUGH ARGUMENTS INCLUDED\n" +
             "\nUSAGE:\njava -jar target/airline-2023.0.0.jar [options] \"Airline Name\" " +
             "FlightNumber Source DepartureTime DepartureDate Destination ArrivalTime ArrivalDate"));
   }
