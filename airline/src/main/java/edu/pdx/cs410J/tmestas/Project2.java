334c42e77fc9257cfc9378cf6f53b02a263b5cd1
Tanner <tmestas@pdx.edu>
2023-01-31 15:47:58 -0800

    took out check

--- airline/src/main/java/edu/pdx/cs410J/tmestas/Project2.java
+++ airline/src/main/java/edu/pdx/cs410J/tmestas/Project2.java
@@ -240,8 +240,11 @@ public class Project2 {
 
     if(textFile){
       filePath = getFilePath(args);
+      /*
       if(isValidFilePath(filePath)){++listSize;}//must account for the extra arg if valid filepath was included
       else{System.out.println("Invalid file path");}
+      */
+       ++listSize;
     } //get textFile path
 
 
