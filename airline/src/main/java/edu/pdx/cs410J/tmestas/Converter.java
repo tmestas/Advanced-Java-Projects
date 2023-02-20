add0e8099440da9d03fbd5e923b87fb3b78708e1
Tanner <tmestas@pdx.edu>
2023-02-20 13:43:02 -0800

    Added functionality to Converter, updated README.txt

--- airline/src/main/java/edu/pdx/cs410J/tmestas/Converter.java
+++ airline/src/main/java/edu/pdx/cs410J/tmestas/Converter.java
@@ -1,34 +1,47 @@
 package edu.pdx.cs410J.tmestas;
-
 import edu.pdx.cs410J.ParserException;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
 import java.util.Collections;
 
 public class Converter {
     public static void main(String[] args){
 
-        //String textFilePath = args[0];
-        //String xmlFilePath = args[1];
+        //usage: java -cp target/airline-2023.0.0.jar edu.pdx.cs410J.tmestas.Converter <Text File Path> <XML File Path>
 
-        String textFilePath = "C:\\Users\\tanne\\Desktop\\seipp.txt";
-        String xmlFilePath = "C:\\Users\\tanne\\Desktop\\seipp.xml";
+        if(args.length > 2) {
+            System.out.println("Too many arguments included, run -README for instructions");
+            return;
+        }
+
+        if(args.length < 2){
+            System.out.println("Not enough arguments included, run -README for instructions");
+            return;
+        }
+
+        String textFilePath = args[0];
+        String xmlFilePath = args[1];
         Airline newAirline;
+        TextParser parser;
 
         try {
             FileReader f = new FileReader(textFilePath);
             BufferedReader b = new BufferedReader(f);
-            TextParser parser = new TextParser(b);
+            parser = new TextParser(b);
+        }catch(FileNotFoundException e){
+            System.out.println("Text file does not exist at this location, exiting");
+            return;
+        }
+
+        try{
             XmlDumper dumper = new XmlDumper(xmlFilePath);
             newAirline = parser.parse();
             Collections.sort(newAirline.getFlights());
             dumper.dump(newAirline);
-        }catch(FileNotFoundException e){
-            System.out.println("Text file does not exist at this location");
-        }
-        catch(Exception e){
+        }catch (IOException e){
+            System.out.println("Could not access directory for XML file, exiting");
+        }catch (ParserException e){
             System.out.println("Text file is malformed");
         }
     }
--- airline/src/main/resources/edu/pdx/cs410J/tmestas/README.txt
+++ airline/src/main/resources/edu/pdx/cs410J/tmestas/README.txt
@@ -1,6 +1,6 @@
 
 Name: Tanner Mestas
-Assignment: Advanced Programming with Java - Project 3
+Assignment: Advanced Programming with Java - Project 4
 
 The purpose of this assignment is to provide a command line interface for a user to add flight objects to
 airline objects, and perform different actions with the data.
@@ -15,16 +15,18 @@ Options:
 -pretty <FilePath>      | Pretty prints airline and its flights to a text file
 -pretty -               | Pretty prints airline and its flights to standard out
 -pretty <FilePath> -    | Pretty prints airline and its flights to a text file and standard out
+-XmlFile <FilePath>     | Prints the provided flight and airline to a Xml file (if valid), appends to file
+                        | if there already exists a file, aborts if airlines in file and command line differ
 
 *****************************************************************************************************************
 
 Args (in order):
 
 Flight Number: Must be a parseable integer
-Source Airport Code: Must be a 3-letter code that corresponds to an existing airport
+Source Airport Code: Must be a 3-letter code that corresponds to a stored airport
 Departure Date: Date the flight will depart in format dd/mm/yyyy
 Departure Time: Time the flight will depart in format hh:mm a
-Destination Airport Code: Must be a 3-letter code that corresponds to an existing airport
+Destination Airport Code: Must be a 3-letter code that corresponds to a stored airport
 Arrival Date: Date the flight will arrive in format dd/mm/yyyy
 Arrival Time: Time the flight will arrive in format hh:mm a
 
